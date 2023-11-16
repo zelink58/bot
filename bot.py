28cc58e5ab851fa7d5b5de905f185a9271c1c361
zelink58 <135225027+zelink58@users.noreply.github.com>
2023-11-16 19:03:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,3 +4,8 @@ import parser
 #main variables
 TOKEN = "6892263065:AAHDiIdT4SaTluUVY6skMRAXpWz9C3Vtmw4"
 bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start', 'go'])
+def start_handler(message):
+    bot.send_message(message.chat.id, 'Привет, когда я вырасту, я буду парсить заголовки с Хабра')
+bot.polling()
